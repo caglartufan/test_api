e666bbc9ab2b5107c8edecf5447e4bd32815ff75
n3pixowe <n3pixowe@gmail.com>
2021-05-19 01:17:04 +0300

    apidoc updated

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,6 +1,6 @@
 define({
   "name": "Test API",
-  "version": "0.1.0",
+  "version": "1.0.0",
   "description": "Test API için dokümantasyon",
   "title": "Test API — Dokümantasyon",
   "url": "http://localhost:3000/api",
@@ -14,7 +14,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-05-15T14:10:44.727Z",
+    "time": "2021-05-18T22:16:44.176Z",
     "url": "https://apidocjs.com",
     "version": "0.27.1"
   }
